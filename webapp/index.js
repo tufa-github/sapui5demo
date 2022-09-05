346b37e7bc97c970177713412d2883c7b0ae5ed0
tufa <zhengwu0512@gmail.com>
2022-09-05 15:25:23 +0200

    component config

--- webapp/index.js
+++ webapp/index.js
@@ -1,27 +1,13 @@
-/*
 sap.ui.define([
-        "sap/m/Text"
-       
-], function (Text) {
+	"sap/ui/core/ComponentContainer"
+], function (ComponentContainer) {
 	"use strict";
 
-
-	new Text({
-		text: "Hello World!"
+	new ComponentContainer({
+		name: "sap.ui.demo.walkthrough",
+		settings : {
+			id : "walkthrough"
+		},
+		async: true
 	}).placeAt("content");
-});
-*/
-
-
-sap.ui.define([
-	"sap/ui/core/mvc/XMLView"
-], function (XMLView) {
-	"use strict";
-
-	XMLView.create({
-		viewName: "sap.ui.demo.walkthrough.view.App"
-	}).then(function (oView) {
-		oView.placeAt("content");
-	});
-
 });
